f44ea5e598e7268e736d70b0fc1b4d2c87475272
yurtsa <splate@inbox.ru>
2023-03-10 14:07:11 +0300

    first commit

---
+++ config/init.php
@@ -0,0 +1,17 @@
+<?php
+
+define("DEBUG", 1);
+define("ROOT", dirname(__DIR__));
+define("WWW", ROOT . '/public');
+define("APP", ROOT . '/app');
+define("CORE", ROOT . '/vendor/wfm');
+define("HELPERS", ROOT . '/vendor/wfm/helpers');
+define("CACHE", ROOT . '/tmp/cache');
+define("LOGS", ROOT . '/tmp/logs');
+define("CONFIG", ROOT . '/config');
+define("LAYOUT", 'ishop');
+define("PATH", 'http://localhost');
+define("ADMIN", 'http://localhost/admin');
+define("NO_IMAGE", 'uploads/no_image.jpg');
+
+require_once ROOT . '/vendor/autoload.php';
--- public/index.php
+++ public/index.php
@@ -1,2 +1,8 @@
 <?php
 echo 'it\'s worker';
+if (PHP_MAJOR_VERSION < 8) {
+    exit('Требуется версия PHP 8');
+}
+
+
+require_once dirname(__DIR__) . '/config/init.php';
